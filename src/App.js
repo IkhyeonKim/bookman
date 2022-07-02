06c474d006104b0f2a347b315ef9b875a7dd02b1
ikhyeon.kim <kih199212@gmail.com>
2022-07-02 17:48:52 +0900

    Fist commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { watchServer } from "./apis";
+import MainPage from "./components/pages/mainPage";
+import "antd/dist/antd.css";
+
+const useEventSource = (url) => {
+  const [data, updateData] = useState(null);
+
+  useEffect(() => {
+    const source = new EventSource(url);
+
+    source.onmessage = function logEvents(event) {
+      console.log(event);
+      updateData(JSON.parse(event.data));
+    };
+  }, [url]);
+
+  return data;
+};
 
 function App() {
+  const DEPENDENCY = "something";
+  const [message, setMessage] = useState("");
+  // const data = useEventSource("https://bookman.brique.kr:8080/v1/sse");
+  useEffect(() => {
+    const eventSource = new EventSource("https://bookman.brique.kr:8080/v1/sse");
+
+    eventSource.onopen = (event) => {
+      console.log("server is opned", event);
+    };
+
+    eventSource.onmessage = (event) => {
+      console.log("server sent a message", { event });
+      setMessage(JSON.parse(event.data));
+    };
+
+    eventSource.onerror = (event) => {
+      console.log("Server sent an error", { event });
+    };
+
+    eventSource.addEventListener("START_RENTAL", (e) => {
+      console.log("START_RENTAL", { e });
+      const data = JSON.parse(e.data);
+      setMessage(data.status);
+    });
+
+    eventSource.addEventListener("END_RENTAL", (e) => {
+      console.log("END_RENTAL", { e });
+      const data = JSON.parse(e.data);
+      setMessage(data.status);
+    });
+
+    return () => eventSource.close();
+  }, [DEPENDENCY]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello World
+      <MainPage />
+      <div>{message}</div>
     </div>
   );
 }
